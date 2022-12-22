13cc88f2b1e8b12e1f744cc30602b5a2cd856a26
katherinemakarevich <114359487+katherinemakarevich@users.noreply.github.com>
2022-12-22 21:47:30 +0300

    Update 3a-t4.cpp

--- 3a-t4.cpp
+++ 3a-t4.cpp
@@ -5,13 +5,15 @@ int main()
 {
 int k, i = 1, sum_k = 0, number;
 cout << " Enter the amount of integers to sum = "; cin >> k;
-  if (k==0)
-    cout << "it can not work with zero" << endl;
-  else
-  {
 do {
 cout << "Enter integer nr. " << i << ": "; 
 cin >> number;
+  while (number-(int)number!=0)
+  { cout << "u supposed to enter integers" << endl;
+   cout << "try again =";
+   cin >> number;
+   break;
+  }
 sum_k += number; i++;
 } while (i <= k);
 cout << " The total sum of " << k << " integers is: "  << sum_k;
